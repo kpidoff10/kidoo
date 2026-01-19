7aa471c486d4740b79979549152073c5340bd887
kpidoff-portable <k.pidoff@gmail.com>
2026-01-19 22:02:42 +0100

    clean

--- esp32/src/managers/commands/storage/command.h
+++
@@ -1,9 +0,0 @@
-#ifndef STORAGE_COMMAND_H
-#define STORAGE_COMMAND_H
-
-#include <Arduino.h>
-#include <ArduinoJson.h>
-
-bool handleGetStorageCommand(JsonDocument& doc);
-
-#endif // STORAGE_COMMAND_H
